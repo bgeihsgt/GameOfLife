c313e74ede4d63d160d97003173c8a3362fb1297
Brian Geihsler <brian@bitgeyser.net>
2014-07-12 01:10:14 -0700

    Add linting to the build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,40 @@
 var gulp = require('gulp'),
-	mocha = require('gulp-mocha');
+	mocha = require('gulp-mocha'),
+	jshint = require('gulp-jshint'),
 	should = require('should'),
 	watchingTests = false;
 
 
 
 function handleTestError(err) {
-	console.log(err.toString());
-	
 	if (watchingTests) {
 		this.emit('end');
 		return;
 	}
-
+	
+	console.log(err.toString());
 	process.exit(1);
 }
 
 
-gulp.task('test', function() {
-	return gulp.src('tests/**/*.spec.js', { read: false })
-		.pipe(mocha({ reporter: 'list', globals: [should]}).on('error', handleTestError));
-		
+gulp.task('lint', function() {
+	return gulp.src(['tests/**/*.js','app/**/*.js', 'gulpfile.js'])
+			.pipe(jshint())
+			.pipe(jshint.reporter('default'))
+			.pipe(jshint.reporter('fail'));
+
+});
+
+gulp.task('test', ['lint'], function() {
+	return gulp.src(['tests/**/*.js','app/**/*.js'], { read: false })
+		.pipe(mocha({ reporter: 'list', globals: [should]}).on('error', handleTestError));	
 
 });
 
 gulp.task('watch-tests', function() {
 	watchingTests = true;
 	return gulp.watch('tests/**', ['test']);
-})
+});
 
 gulp.task('build', ['test'], function() {
 	return gulp.src('app/index.html')
