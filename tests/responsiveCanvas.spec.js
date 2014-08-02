2f69a2751dffa50dd7fa2923a29efe127f329315
Brian Geihsler <brian@bitgeyser.net>
2014-08-01 23:50:28 -0700

    Add spec for a responsive canvas control

---
+++ tests/responsiveCanvas.spec.js
@@ -0,0 +1,11 @@
+describe('A responsive canvas', function() {
+
+	describe('when initialized', function() {
+
+		it('should create the canvas element inside the container', function() {
+
+		});
+
+	});
+
+});
